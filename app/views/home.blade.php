bf28fc1efc35c4d2b2c58c1393785e2c781df982
qu1ckkkk <theninjabag@gmail.com>
2014-04-15 15:58:36 +0200

    Get *something* going for a index page

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,17 +5,42 @@
 @section('page_content')
 
 <div class="row">
+    <div class="col-md-8">
+        <a href="{{ action('ApiKeyController@getNewKey') }}" type="button" class="btn btn-primary btn-lg"><i class="fa fa-plus"></i> Add a new API Key</a>
+        <a href="{{ action('ApiKeyController@getAll') }}" type="button" class="btn btn-success btn-lg"><i class="fa fa-list"></i> List All Keys</a>
+    </div>
+    <div class="col-md-4">
+        <dl class="dl-horizontal">
+            <dt>Server Online</dt>
+            <dd>{{ $server->serverOpen }}</dd>
+            <dt>Online Players</dt>
+            <dd>{{ $server->onlinePlayers }}</dd>
+            <dt>Last Checked</dt>
+            <dd>{{ Carbon\Carbon::parse($server->currentTime)->diffForHumans() }}</dd>
+        </dl>
+    </div>
+</div>
+
+<hr>
+
+<div class="row">
     <div class="col-lg-3 col-xs-6">
         <!-- small box -->
         <div class="small-box bg-blue">
             <div class="inner">
                 <h3>
-                	{{ Carbon\Carbon::parse($server->currentTime)->diffForHumans() }}
+                    {{ $total_keys }}
                 </h3>
                 <p>
-                    Last Server Status Check
+                    Total Recorded API Keys
                 </p>
             </div>
+            <div class="icon">
+                <i class="fa fa-key"></i>
+            </div>
+            <a href="{{ action('ApiKeyController@getAll') }}" class="small-box-footer">
+                All Keys <i class="fa fa-arrow-circle-right"></i>
+            </a>
         </div>
     </div><!-- ./col -->
     <div class="col-lg-3 col-xs-6">
@@ -23,26 +48,58 @@
         <div class="small-box bg-purple">
             <div class="inner">
                 <h3>
-                    {{ $server->onlinePlayers }}
+                   {{ $total_characters }} 
                 </h3>
                 <p>
-                    Online Players
+                    Total Character Sheets
                 </p>
             </div>
+            <div class="icon">
+                <i class="fa fa-users"></i>
+            </div>
+            <a href="{{ action('CharacterController@getAll') }}" class="small-box-footer">
+                All Characters <i class="fa fa-arrow-circle-right"></i>
+            </a>
         </div>
     </div><!-- ./col -->
     <div class="col-lg-3 col-xs-6">
         <!-- small box -->
-        <div class="small-box bg-teal">
+        <div class="small-box bg-green">
             <div class="inner">
                 <h3>
-                    {{ $server->serverOpen }}
+                    {{ number_format($total_isk , 2, '.', ' ') }}
                 </h3>
                 <p>
-                	Server Online?  
+                    Total Recorded ISK
                 </p>
             </div>
+            <div class="icon">
+                <i class="fa fa-money"></i>
+            </div>
+            <a href="{{ action('CharacterController@getAll') }}" class="small-box-footer">
+                All Characters <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div><!-- ./col -->
+    <div class="col-lg-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-maroon">
+            <div class="inner">
+                <h3>
+                    {{ number_format($total_skillpoints , 0, '.', ' ') }}
+                </h3>
+                <p>
+                    Total Recorded Skillpoints
+                </p>
+            </div>
+            <div class="icon">
+                <i class="fa fa-align-justify"></i>
+            </div>
+            <a href="{{ action('CharacterController@getAll') }}" class="small-box-footer">
+                All Characters <i class="fa fa-arrow-circle-right"></i>
+            </a>
         </div>
     </div><!-- ./col -->
 </div>
+
 @stop
