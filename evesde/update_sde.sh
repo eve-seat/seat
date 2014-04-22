20fa7d7b810bfad2210b48a2dcbfcfc51c441fce
Stephen Swatman <stephenswat@gmail.com>
2014-04-22 20:32:35 +0200

    Update SDE updater with flag and database check
    Implemented a check in the SDE updater which checks for valid database
    connections and add an option for users to not have it attempt to
    automatically import the SQL files.

--- evesde/update_sde.sh
+++ evesde/update_sde.sh
@@ -1,44 +1,95 @@
 #!/bin/bash
 
-# 2013 - Leon Jacobs
+# 2014 - Leon Jacobs
 # SeAT
 
 # This script should pull the latest static data as per $DUMPS from
-# https://www.fuzzwork.co.uk/dump/ based in the expansion parameter
+# https://www.fuzzwork.co.uk/dump/ based in the expansion parameter.
+# It will also immediately insert them into the database.
+
+# Ask function borrowed from davejamesmiller, for more information check out
+# the following gist: https://gist.github.com/davejamesmiller/1965569
+function ask {
+	while true; do
+
+		if [ "${2:-}" = "Y" ]; then
+			prompt="Y/n"
+			default=Y
+		elif [ "${2:-}" = "N" ]; then
+			prompt="y/N"
+			default=N
+		else
+			prompt="y/n"
+			default=
+		fi
+
+		read -p "$1 [$prompt] " REPLY
+
+		if [ -z "$REPLY" ]; then
+			REPLY=$default
+		fi
+
+		case "$REPLY" in
+			Y*|y*) return 0 ;;
+			N*|n*) return 1 ;;
+		esac
+
+	done
+}
 
 URL="https://www.fuzzwork.co.uk/dump/"
 EXPANSION="rubicon-1.3-95173"
 EXTENTION="sql"
 DUMPS="required_sde"
-TEMPFILE=/tmp/SeAT-$(date | md5sum | awk '{ print $1 }')
+TEMP=/tmp/SeAT-$(date | md5sum | awk '{ print $1 }')
+AUTO_IMPORT=$(ask "Automatically import required SQL files?" N)
 
-# check how many lines in $DUMPS for a form of progress
-echo "We have $(wc -l $DUMPS | awk '{ print $1 }') static datadump files to do."
-COUNT=$(wc -l $DUMPS | awk '{ print $1 }')
-PROGRESS=1
+if [ AUTO_IMPORT ]; then
+	read -p "Enter database user: " DB_USR
+	read -s -p "Enter database password: " DB_PSS
+	echo ""
+	read -p "Enter database server (empty for localhost): " DB_SRV
 
-# make the working directory in /tmp
-echo "Creating temp directory $TEMPFILE"
-mkdir -p $TEMPFILE
+	if [ -z "$DB_SRV" ]; then
+			DB_SRV="localhost"
+	fi
 
-# download each files
+	read -p "Enter database name: " DB_NAME
+
+	until mysql -e ";" -u $DB_USR --password=$DB_PSS -h $DB_SRV $DB_NAME; do
+		echo "Database connection failed. Please try again."
+		read -p "Enter database user: " DB_USR
+		read -s -p "Enter database password: " DB_PSS
+		echo ""
+		read -p "Enter database server (empty for localhost): " DB_SRV
+
+		if [ -z "$DB_SRV" ]; then
+				DB_SRV="localhost"
+		fi
+
+		read -p "Enter database name: " DB_NAME
+	done
+fi
+
+# Make the working directory in /tmp
+echo "Creating temp directory $TEMP..."
+mkdir -p $TEMP
+
+# Download each file
 while read p; do
+	echo "Getting dump $p from $URL$EXPANSION/$p.$EXTENTION.bz2..."
+	curl -s "$URL$EXPANSION/$p.$EXTENTION.bz2" > $TEMP/$p.$EXTENTION.bz2
 
-  echo "$PROGRESS/$COUNT - Getting dump $p from '$URL$EXPANSION/$p.$EXTENTION.bz2' .."
-  curl -s "$URL$EXPANSION/$p.$EXTENTION.bz2" > $TEMPFILE/$p.$EXTENTION.bz2
-
-  echo "        Extracting $TEMPFILE/$p.$EXTENTION.bz2 ..."
-  bzip2 -d $TEMPFILE/$p.$EXTENTION.bz2
-
-  # increment progress
-  PROGRESS=$[PROGRESS + 1]
-
+	echo "Extracting $TEMP/$p.$EXTENTION.bz2 ..."
+	bzip2 -d $TEMP/$p.$EXTENTION.bz2
 done < $DUMPS
 
-# list $TEMPFILE
-echo "Done, here is the contents of $TEMPFILE"
-ls -lah $TEMPFILE
-
-# suggest MySQL import command
-echo "Import the .sql thats in $TEMPFILE with something like:"
-echo "mysql -u seat -p seat < $TEMPFILE/*.sql"
+if [ AUTO_IMPORT ]; then
+	# Import the files into the MySQL server
+	echo "Importing the SQL files into MySQL..."
+	cat $TEMP/*.sql | mysql -u $DB_USR --password=$DB_PSS -h $DB_SRV $DB_NAME
+	# Clean up after ourselves
+	rm $TEMP/* && rmdir $TEMP
+else
+	echo "The extracted files can be found in $TEMP."
+fi
