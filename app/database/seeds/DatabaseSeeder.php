fef9abcbccbf5adfd8b46a1632fd73c03a634570
LunarchildEU <LunarchildEU@users.noreply.github.com>
2014-05-09 01:26:28 +0200

    correct indention

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder {
 
 		$this->call('UserTableSeeder');
 		$this->call('EveApiCalllistTableSeeder');
-        $this->call('EveNotificationTypesSeeder');
+        	$this->call('EveNotificationTypesSeeder');
 		$this->call('EveCorporationRolemapSeeder');
 	}
 }
