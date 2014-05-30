eee1becfaeb93c82f5c6675167e1ea6ee21d4d5a
omgninjaz <ralph@omgninjaz.com>
2014-05-30 12:16:03 -0400

    add a nil check on the character view page for when skill queue is empty

--- app/views/character/view.blade.php
+++ app/views/character/view.blade.php
@@ -189,12 +189,14 @@
 
 		                                <dt>Skill Queue Ends</dt>
 		                                <dd>
-		                                	{{ end($skill_queue)->endTime }} ({{ Carbon\Carbon::parse(end($skill_queue)->endTime)->diffForHumans() }})
-		                                	@if (Carbon\Carbon::parse(end($skill_queue)->endTime)->lte(Carbon\Carbon::now()->addDay()))
-		                                		<span class="text-yellow"><i class="fa fa-exclamation"></i> Less than 24hrs worth of skill queue left</span>
-		                                	@else
-		                                		<span class="text-green"><i class="fa fa-check"></i> More than 24hrs worth of skill queue left</span>
-		                                	@endif
+		                                	@if ($skill_queue)
+			                                	{{ end($skill_queue)->endTime }} ({{ Carbon\Carbon::parse(end($skill_queue)->endTime)->diffForHumans() }})
+			                                	@if (Carbon\Carbon::parse(end($skill_queue)->endTime)->lte(Carbon\Carbon::now()->addDay()))
+			                                		<span class="text-yellow"><i class="fa fa-exclamation"></i> Less than 24hrs worth of skill queue left</span>
+			                                	@else
+			                                		<span class="text-green"><i class="fa fa-check"></i> More than 24hrs worth of skill queue left</span>
+			                                	@endif
+			                                @endif
 		                                </dd>
 		                            </dl>
 		                        </div><!-- /.box-body -->
