9ef9422071f61d480872bf115f6df228b4e5cfa1
qu1ckkkk <theninjabag@gmail.com>
2014-06-04 11:15:22 +0200

    Fix incorrect variable reference

--- app/views/queue/view.blade.php
+++ app/views/queue/view.blade.php
@@ -18,7 +18,7 @@
 				<li><b>OwnerID: </b>{{ $detail->ownerID }}</li>
 				<li><b>API: </b>{{ $detail->api }}</li>
 				<li><b>Scope: </b>{{ $detail->scope }}</li>
-				<li><b>Status: </b>{{ $detail->scope }}</li>
+				<li><b>Status: </b>{{ $detail->status }}</li>
 				<li><b>Created At: </b>{{ $detail->created_at }} ({{ Carbon\Carbon::parse($detail->created_at)->diffForHumans() }})</li>
 				<li><b>Last Updated At: </b>{{ $detail->updated_at }} ({{ Carbon\Carbon::parse($detail->updated_at)->diffForHumans() }})</li>
 			</ul>
