70ef2389ce3020311e39d0dfb4e267baf3ea257c
qu1ckkkk <theninjabag@gmail.com>
2014-06-06 09:08:18 +0200

    Really fix up user identifier message

--- app/views/layouts/components/header.blade.php
+++ app/views/layouts/components/header.blade.php
@@ -38,7 +38,7 @@
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="glyphicon glyphicon-user"></i>
-                        <span>{{ Sentry::getUser()->first_name }} <i class="caret"></i></span>
+                        <span>{{ Sentry::getUser()->email }} <i class="caret"></i></span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
