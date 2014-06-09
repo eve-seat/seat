eab9f4d8ede255d941ba2420d085b745e65c64c9
gregoryduckworth <gregoryduckworth@googlemail.com>
2014-06-09 10:29:07 +0100

    item search

---
+++ app/views/eve/item/ajax/result.blade.php
@@ -0,0 +1,31 @@
+@if (count($items) > 0)
+	<table class="table table-condensed table-hover" id="datatable">
+	    <thead>
+	    	<tr>
+	    			<th>#</th>
+		        <th>TypeID</th>
+		        <th>TypeName</th>
+		        <th>Description</th>
+		        <th>Capacity</th>
+		        <th>Volume</th>
+		    </tr>
+	    </thead>
+	    <tbody>
+		    @foreach ($items as $result)
+			    <tr>
+			    		<td><img src='//image.eveonline.com/Type/{{ $result->typeID }}_32.png' style='width: 18px;height: 18px;'></td>
+			        <td>{{ $result->typeID }}</td>
+			        <td>{{ $result->typeName }}</td>			   
+			        <td>{{ $result->description }}</td>
+			        <td>{{ number_format($result->capacity, 0, '.', ' ') }}</td>
+			        <td>{{ number_format($result->volume, 0, '.', ' ') }}</td>
+			    </tr>
+			@endforeach
+		</tbody>
+	</table>
+@else
+	<div class="callout callout-warning">
+	    <h4>No Results</h4>
+	    <p>Your asset specific search yielded no results.</p>
+	</div>
+@endif
