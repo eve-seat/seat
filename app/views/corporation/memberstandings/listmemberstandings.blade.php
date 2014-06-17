f3440982daad6c04dd0d3b1d771b336719895703
Greg Duckworth <gregoryduckworth@googlemail.com>
2014-06-17 10:26:37 +0100

    standing selection page to fall in line with others

--- app/views/corporation/memberstandings/listmemberstandings.blade.php
+++ app/views/corporation/memberstandings/listmemberstandings.blade.php
@@ -5,18 +5,23 @@
 @section('page_content')
 
 @foreach ($corporations as $corp)
-    <div class="small-box bg-blue col-md-4">
-        <div class="inner">
-            <h3>
-                {{ $corp->corporationName }}
-            </h3>
-            <p>
-                From character: {{ $corp->characterName }}
-            </p>
+    <div class="col-md-4">
+        <div class="small-box bg-blue">
+            <div class="inner">
+                <h3>
+                    {{ $corp->corporationName }}
+                </h3>
+                <p>
+                    From character: {{ $corp->characterName }}
+                </p>
+            </div>
+            <div class="icon">
+                <img src="http://image.eveonline.com/Corporation/{{ $corp->corporationID }}_32.png" class="img-circle" />
+            </div>
+            <a href="{{ action('CorporationController@getMemberStandings', array('corporationID' => $corp->corporationID)) }}" class="small-box-footer">
+                View Standings <i class="fa fa-arrow-circle-right"></i>
+            </a>
         </div>
-        <a href="{{ action('CorporationController@getMemberStandings', array('corporationID' => $corp->corporationID)) }}" class="small-box-footer">
-            View Standings <i class="fa fa-arrow-circle-right"></i>
-        </a>
     </div>
 @endforeach
 
