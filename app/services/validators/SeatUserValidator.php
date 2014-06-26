91ff1a4ab4a78204fd80ae4f7b2b51403328e421
Greg Duckworth <gregoryduckworth@googlemail.com>
2014-06-26 10:44:58 +0100

    validator for registering

--- app/services/validators/SeatUserValidator.php
+++ app/services/validators/SeatUserValidator.php
@@ -4,6 +4,7 @@ class SeatUserValidator extends Validator {
  
     public static $rules = array(
         'email' => 'required|email',
+        'username' => 'required|min:6',
         'password'  => 'required|min:6',
     );
  
