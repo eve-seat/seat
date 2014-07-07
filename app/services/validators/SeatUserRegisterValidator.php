388fe6141211caf869bc62437a75b75332e5a2e9
qu1ckkkk <theninjabag@gmail.com>
2014-07-07 11:46:43 +0200

    Remove minimum 6 chars for username validation

--- app/services/validators/SeatUserRegisterValidator.php
+++ app/services/validators/SeatUserRegisterValidator.php
@@ -4,7 +4,7 @@ class SeatUserRegisterValidator extends Validator {
  
     public static $rules = array(
         'email' => 'required|email|unique:users',
-        'username' => 'required|min:6|unique:users',
+        'username' => 'required|unique:users',
         'password'  => 'required|min:6|confirmed',
         'password_confirmation'  => 'required|min:6',
     );
