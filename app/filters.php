48d2f6227973230106e6c5fc87bee7b1120f34cc
qu1ckkkk <theninjabag@gmail.com>
2014-12-06 10:16:05 +0200

    Remove annoying warning ^_^

--- app/filters.php
+++ app/filters.php
@@ -81,8 +81,7 @@ App::after(function($request, $response)
 Route::filter('auth', function()
 {
 	if (!Sentry::check())
-		return Redirect::action('SessionController@getSignIn')
-			->with('warning', 'Please Sign In first to continue');
+		return Redirect::action('SessionController@getSignIn');
 });
 
 
