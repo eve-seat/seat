c03c42ebf2756efaa4f189933e95f022166adc54
qu1ckkkk <theninjabag@gmail.com>
2014-12-09 10:46:15 +0200

    Add a few more searches for #172

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -35,7 +35,12 @@ class DashboardController extends BaseController {
     {
         if (Request::ajax()) {
 
-            // Process the searches!
+            /*
+            |--------------------------------------------------------------------------
+            | Search Characters
+            |--------------------------------------------------------------------------
+            */
+
             $characters = DB::table('account_apikeyinfo_characters')
                 ->leftJoin('seat_keys', 'account_apikeyinfo_characters.keyID', '=', 'seat_keys.keyID')
                 ->join('character_charactersheet', 'account_apikeyinfo_characters.characterID', '=', 'character_charactersheet.characterID')
@@ -52,8 +57,101 @@ class DashboardController extends BaseController {
             else
                 $characters = $characters->get();
 
+            /*
+            |--------------------------------------------------------------------------
+            | Search Character Assets
+            |--------------------------------------------------------------------------
+            */
+
+            $character_assets = DB::table(DB::raw('character_assetlist as a'))
+                ->select(DB::raw(
+                    "*, CASE
+                    when a.locationID BETWEEN 66000000 AND 66014933 then
+                        (SELECT s.stationName FROM staStations AS s
+                          WHERE s.stationID=a.locationID-6000001)
+                    when a.locationID BETWEEN 66014934 AND 67999999 then
+                        (SELECT c.stationName FROM `eve_conquerablestationlist` AS c
+                          WHERE c.stationID=a.locationID-6000000)
+                    when a.locationID BETWEEN 60014861 AND 60014928 then
+                        (SELECT c.stationName FROM `eve_conquerablestationlist` AS c
+                          WHERE c.stationID=a.locationID)
+                    when a.locationID BETWEEN 60000000 AND 61000000 then
+                        (SELECT s.stationName FROM staStations AS s
+                          WHERE s.stationID=a.locationID)
+                    when a.locationID>=61000000 then
+                        (SELECT c.stationName FROM `eve_conquerablestationlist` AS c
+                          WHERE c.stationID=a.locationID)
+                    else (SELECT m.itemName FROM mapDenormalize AS m
+                        WHERE m.itemID=a.locationID) end
+                        AS location,a.locationId AS locID"))
+                ->join('invTypes', 'a.typeID', '=', 'invTypes.typeID')
+                ->join('account_apikeyinfo_characters', 'account_apikeyinfo_characters.characterID', '=', 'a.characterID');
+
+            // If the user is not a superuser, filter the results down to keys they own
+            if (!Sentry::getUser()->isSuperUser())
+                $character_assets = $character_assets->whereIn('account_apikeyinfo_characters.keyID', Session::get('valid_keys'));
+
+            // Complete the search
+            $character_assets = $character_assets->where('invTypes.typeName', 'like', '%' . Input::get('q') . '%')
+                ->groupBy('a.characterID')
+                ->orderBy('location')
+                ->get();
+
+            /*
+            |--------------------------------------------------------------------------
+            | Search Character Contact Lists
+            |--------------------------------------------------------------------------
+            */
+
+            // Search character contact lists
+            $character_contactlist = DB::table('character_contactlist')
+                ->join('account_apikeyinfo_characters', 'account_apikeyinfo_characters.characterID', '=', 'character_contactlist.characterID')
+                ->where('character_contactlist.contactName', 'like', '%' . Input::get('q') . '%');
+
+            // Ensure we only get result for characters we have access to
+            if (!Sentry::getUser()->hasAccess('recruiter'))
+                $character_contactlist = $character_contactlist->whereIn('account_apikeyinfo_characters.keyID', Session::get('valid_keys'))
+                    ->get();
+            else
+                $character_contactlist = $character_contactlist->get();
+
+            /*
+            |--------------------------------------------------------------------------
+            | Search Character Mail
+            |--------------------------------------------------------------------------
+            */
+
+            $character_mail = DB::table('character_mailmessages')
+                ->join('account_apikeyinfo_characters', 'character_mailmessages.characterID', '=', 'account_apikeyinfo_characters.characterID')
+                ->join('character_mailbodies', 'character_mailmessages.messageID', '=', 'character_mailbodies.messageID')
+                ->where('character_mailmessages.senderName', 'like', '%' . Input::get('q') . '%')
+                ->orWhere('character_mailmessages.title', 'like', '%' . Input::get('q') . '%')
+                ->orWhere('character_mailbodies.body', 'like', '%' . Input::get('q') . '%');
+
+            // Ensure we only get result for characters we have access to
+            if (!Sentry::getUser()->hasAccess('recruiter'))
+                $character_mail = $character_mail->whereIn('account_apikeyinfo_characters.keyID', Session::get('valid_keys'));
+
+            $character_mail = $character_mail
+                ->groupBy('character_mailmessages.messageID')
+                ->orderBy('character_mailmessages.sentDate', 'desc')
+                ->take(50)
+                ->get();
+
+
+            // Return the AJAX response
             return View::make('search')
-                ->with('characters', $characters);
+                ->with('keyword', Input::get('q'))
+                ->with('characters', $characters)
+                ->with('character_assets', $character_assets)
+                ->with('character_contactlist', $character_contactlist)
+                ->with('character_mail', $character_mail)
+                ;
+
+        } else {
+
+            // Not a ajax request? Go away :>
+            App::abort(404);
         }
     }
 }
