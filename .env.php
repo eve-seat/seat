f5fb0c88efa7d5556495b8fa052fed522f015cb1
qu1ckkkk <theninjabag@gmail.com>
2014-12-10 13:02:57 +0200

    Introduce the SeAT installer.
    This commit introduces the first iteration of the SeAT installer.
    The installer is able to handle almost everything internal to
    SeAT.

---
+++ .env.php
@@ -0,0 +1,16 @@
+<?php // SeAT Configuration Created: 2014-12-10 10:55:37
+
+return array (
+  'mysql_hostname' => '127.0.0.1',
+  'mysql_database' => 'iseat',
+  'mysql_username' => 'root',
+  'mysql_password' => '',
+  'redis_host' => '127.0.0.1',
+  'redis_port' => 6379,
+  'mail_driver' => 'mail',
+  'mail_from' => 'seatadmin@localhost',
+  'mail_from_name' => 'SeAT Administrator',
+  'smtp_hostname' => '127.0.0.1',
+  'smtp_username' => NULL,
+  'smtp_password' => NULL,
+);
