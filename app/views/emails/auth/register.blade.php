3dfe22281aab18e3fb479da1c6dd6ad6f308a238
qu1ckkkk <theninjabag@gmail.com>
2015-01-03 11:29:17 +0200

    Start making email more pretty

--- app/views/emails/auth/register.blade.php
+++ app/views/emails/auth/register.blade.php
@@ -1,21 +1,31 @@
-<!DOCTYPE html>
-<html lang="en-US">
-  <head>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h2>SeAT Account Creation</h2>
+@extends('emails.layouts.masterLayout')
 
-    <div>
-      <p>
-        Your email address has been used to register for a new SeAT account.
-        To complete the registration, please click this link <br>
-        {{ URL::action('RegisterController@getActivate', array($activation_code)) }}
-      </p>
-      <p>
-        If you did not sign up for this service, then you may safely ignore this email.
-      </p>
+@section('email_title', 'SeAT Account Creation')
 
-    </div>
-  </body>
-</html>
+@section('email_content')
+  <p style="Margin-top: 0;font-weight: 400;color: #fff;font-family: sans-serif;font-size: 14px;line-height: 22px;Margin-bottom: 20px">
+    Hello,
+  </p>
+  <p style="Margin-top: 0;font-weight: 400;color: #fff;font-family: sans-serif;font-size: 14px;line-height: 22px;Margin-bottom: 20px">
+    Your email address has been used to register for a new SeAT account. To complete registration, please click the below link. If this was not requested by you then you may safely disregard this email.
+  </p>
+@stop
+
+@section('button_action')
+<div class="btn" style="Margin-bottom: 20px;text-align: left">
+  <![if !mso]>
+    <a
+      style="padding-top: 15px;padding-bottom: 15px;font-weight: 500;display: inline-block;font-size: 16px;line-height: 20px;text-align: center;text-decoration: none;color: #fff;transition: background-color 0.2s;background-color: #3d88fd;border-bottom: 3px solid #1d2227;font-family: sans-serif;width: 480px;padding-left: 20px;padding-right: 20px"
+      href="{{ URL::action('RegisterController@getActivate', array($activation_code)) }}"
+    >
+      Activate My Account Now
+    </a>
+  <![endif]>
+  <!--[if mso]>
+    <v:rect xmlns:v="urn:schemas-microsoft-com:vml" href="{{ URL::action('RegisterController@getActivate', array($activation_code)) }}" style="width:520px" fillcolor="#3D88FD" stroke="f"><v:shadow on="t" color="#1D2227" on="t" offset="0,3px"></v:shadow><v:textbox style="mso-fit-shape-to-text:t" inset="0px,15px,0px,15px"><center style="font-size:16px;line-height:20px;color:#FFFFFF;font-family:sans-serif;font-weight:500;mso-line-height-rule:exactly;mso-text-raise:1px">
+      Activate My Account Now
+    </center></v:textbox></v:rect>
+  <![endif]-->
+</div>
+
+@stop
---
+++ app/views/emails/layouts/masterLayout.blade.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+      @include('emails.layouts.style')
+
+    <meta name="robots" content="noindex,nofollow" />
+    <meta property="og:title" content="SeAT Email" />
+  </head>
+  <body style="margin: 0;mso-line-height-rule: exactly;padding: 0;min-width: 100%">
+
+    <style type="text/css">
+      @import url(https://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic);@import url(https://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic);@import url(https://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic);@import url(https://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic);@import url(https://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic);@import url(https://fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic);.wrapper,.header,.separator,.one-col-bg,.two-col-bg,.three-col-bg,.one-col-feature-bg{background-color:#1e1e1e}h1{color:#3d88fd}.wrapper h1{}.wrapper h1{font-family:sans-serif}@media only screen and (min-width: 0){.wrapper h1{font-family:Ubuntu,sans-serif !important}}h1{}.one-col h1{line-height:44px}.two-col h1{line-height:44px}.three-col
+      h1{line-height:28px}h2{color:#3d88fd}.wrapper h2{}.wrapper h2{font-family:sans-serif}@media only screen and (min-width: 0){.wrapper h2{font-family:Ubuntu,sans-serif !important}}h2{}.one-col h2{line-height:28px}.two-col h2{line-height:28px}.three-col h2{line-height:22px}h3{color:#3d88fd}.wrapper h3{}.wrapper h3{font-family:sans-serif}@media only screen and (min-width: 0){.wrapper h3{font-family:Ubuntu,sans-serif !important}}h3{}.one-col h3{line-height:28px}.two-col h3{line-height:22px}.three-col h3{line-height:20px}h1 a,h2 a,h3 a{border-bottom:1px solid #3d88fd}p,ol,ul{color:#fff}.wrapper p,.wrapper ol,.wrapper ul{}.wrapper p,.wrapper ol,.wrapper ul{font-family:sans-serif}@media only screen and (min-width: 0){.wrapper p,.wrapper ol,.wrapper ul{font-family:Ubuntu,sans-serif !important}}p,ol,ul{}.one-col p,.one-col ol,.one-col ul{line-height:22px;Margin-bottom:20px}.two-col p,.two-col
+      ol,.two-col ul{line-height:22px;Margin-bottom:20px}.three-col p,.three-col ol,.three-col ul{line-height:22px;Margin-bottom:20px}.hr hr{color:#fff;background-color:#fff}.border{background-color:#161616}.wrapper a{color:#3d88fd}.wrapper a:hover{color:#0b69fc !important}.wrapper .btn a{background-color:#3d88fd;color:#fff;border-bottom:3px solid #1d2227}.wrapper .btn a{font-family:sans-serif}@media only screen and (min-width: 0){.wrapper .btn a{font-family:Ubuntu,sans-serif !important}}.wrapper .btn a:hover{background-color:#2479fd !important;color:#fff !important}.wrapper .logo{color:#e6e6e6}.wrapper .logo{font-family:sans-serif}@media only screen and (min-width: 0){.wrapper .logo{font-family:Ubuntu,sans-serif !important}}.wrapper .logo a{color:#e6e6e6}.wrapper .logo a:hover{color:#e6e6e6 !important}.preheader{background-color:#161616}.preheader .title,.preheader
+      .webversion{color:#e6e6e6}.preheader .title,.preheader .webversion{font-family:sans-serif}@media only screen and (min-width: 0){.preheader .title,.preheader .webversion{font-family:Ubuntu,sans-serif !important}}.preheader .title a,.preheader .webversion a{font-weight:bold;color:#e6e6e6}.preheader .title a:hover,.preheader .webversion a:hover{color:#e6e6e6 !important}.preheader-buffer{background-color:#1e1e1e}.contents{background-color:#212a32}.one-col-feature a{border-bottom:1px solid #3d88fd}.extra-wide .big-feature{background-color:#212a32}.footer,.emb-editor-canvas{background-color:#161616}.footer td{color:#e6e6e6}.footer td{font-family:sans-serif}@media only screen and (min-width: 0){.footer td{font-family:Ubuntu,sans-serif !important}}.wrapper .footer a{font-weight:bold;color:#e6e6e6}.wrapper .footer a:hover{color:#e6e6e6 !important}.social,.social a{color:#e6e6e6}@media
+      (min-width:0){body{background-color:#161616}}@media (-webkit-min-device-pixel-ratio:1.5),(min-resolution:144dpi){.one-col ul{border-left:30px solid #212a32}}
+    </style>
+
+    <center class="wrapper" style="display: table;table-layout: fixed;width: 100%;min-width: 620px;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;background-color: #1e1e1e">
+      <table class="preheader" style="border-collapse: collapse;border-spacing: 0;width: 100%;background-color: #161616">
+        <tbody>
+          <tr>
+          </tr>
+          <tr>
+            <td class="preheader-buffer" style="padding: 0;vertical-align: top;background-color: #1e1e1e;font-size: 20px;line-height: 20px" colspan="2">&nbsp;</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table class="one-col-bg" style="border-collapse: collapse;border-spacing: 0;width: 100%;background-color: #1e1e1e">
+        <tbody>
+          <tr>
+            <td style="padding: 0;vertical-align: top" align="center">
+              <table class="one-col centered" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto;width: 600px">
+                <tbody>
+                  <tr>
+                    <td class="column" style="padding: 0;vertical-align: top;text-align: left">
+                      <table class="contents" style="border-collapse: collapse;border-spacing: 0;width: 100%;background-color: #212a32">
+                        <tbody>
+                          <tr>
+                            <td style="padding: 0;vertical-align: top">
+                              <div><div class="column-top" style="font-size: 30px;line-height: 30px">&nbsp;</div></div>
+                              <table style="border-collapse: collapse;border-spacing: 0;width: 100%" width="100%">
+                                <tbody>
+                                  <tr>
+                                    <td class="padded" style="padding: 0;vertical-align: top;padding-left: 40px;padding-right: 40px;text-align: left">
+
+                                      <h1 style="Margin-top: 0;font-weight: normal;color: #3d88fd;font-family: sans-serif;font-size: 30px;Margin-bottom: 16px;line-height: 44px">
+                                        @if (trim($__env->yieldContent('email_title')))
+                                          @yield('email_title')
+                                        @else
+                                          Untitled Email!
+                                        @endif
+                                      </h1>
+
+                                      {{-- the actual email content goes here --}}
+                                      @yield('email_content')
+
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+
+                              <table style="border-collapse: collapse;border-spacing: 0;width: 100%" width="100%">
+                                <tbody>
+                                  <tr>
+                                    <td class="padded" style="padding: 0;vertical-align: top;padding-left: 40px;padding-right: 40px;text-align: left">
+
+                                      {{-- if the email has botton actions, yield them here --}}
+                                      @yield('button_action')
+
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+
+                              <div class="column-bottom" style="font-size: 10px;line-height: 10px">&nbsp;</div>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div class="separator" style="font-size: 20px;line-height: 20px;background-color: #1e1e1e">&nbsp;</div>
+
+      <table class="footer" style="border-collapse: collapse;border-spacing: 0;table-layout: fixed;width: 100%;background-color: #161616">
+        <tbody>
+          <tr>
+            <td style="padding: 0;vertical-align: top;text-align: left;color: #e6e6e6;font-family: sans-serif">&nbsp;</td>
+            <td class="inner" style="padding: 0;vertical-align: top;text-align: left;color: #e6e6e6;font-family: sans-serif;padding-top: 40px;padding-bottom: 75px;width: 560px">
+              <table style="border-collapse: collapse;border-spacing: 0;width: 100%">
+                <tbody>
+                  <tr>
+                    <td class="campaign" style="padding: 0;vertical-align: top;text-align: left;color: #e6e6e6;font-family: sans-serif;font-size: 12px;line-height: 20px">
+                      {{-- Check that we have a unsubscribe token --}}
+                      @if(isset($unsubscribe_token))
+                        <div>
+                          This email was generated by SeAT {{ Config::get('seat.version') }} at <a href="{{ url('/') }}">{{ url('/') }}</a> on {{ Carbon\Carbon::now() }}<br>
+                          You are receiving this email because you have notifications enabled in your profile.
+                        </div>
+                        <div>
+                          <span>
+                            <span class="block">
+                              <preferences>
+                                Preferences
+                              </preferences>
+                              <span class="hide"> &nbsp;|&nbsp; </span>
+                            </span>
+                          </span>
+                          <span class="block">
+                            <unsubscribe>
+                              Unsubscribe
+                            </unsubscribe>
+                          </span>
+                        </div>
+                      @else
+                        <div>
+                          This email was generated by SeAT {{ Config::get('seat.version') }} at <a href="{{ url('/') }}">{{ url('/') }}</a> on {{ Carbon\Carbon::now() }}</div>
+                      @endif
+                    </td>
+                    <td class="social" style="padding: 0;vertical-align: top;color: #e6e6e6;text-align: left;font-family: sans-serif;text-transform: uppercase;width: 190px">
+                      <div class="padded" style="padding-left: 40px;padding-right: 40px;Margin-left: 20px;padding: 0">
+
+                        <div style="Margin-bottom: 10px">
+                          <a style="color: #e6e6e6;border-bottom: none;display: block;font-size: 10px;letter-spacing: 0.15em;line-height: 12px;text-decoration: none;font-weight: bold" href="https://github.com/eve-seat/seat" target="_blank">
+                            <img style="border: 0;-ms-interpolation-mode: bicubic;vertical-align: middle" src="https://i.imgur.com/8wWKkDx.png" alt="" width="38" height="30">
+                            <span style="mso-text-raise: 11px">SeAT on Github</span>
+                          </a>
+                        </div>
+
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+            <td style="padding: 0;vertical-align: top;text-align: left;color: #e6e6e6;font-family: sans-serif">&nbsp;</td>
+          </tr>
+        </tbody>
+      </table>
+    </center>
+
+  </body>
+</html>
