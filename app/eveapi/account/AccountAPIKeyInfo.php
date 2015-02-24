6b9857a882f369d12a69ef6fc2b320e7576ed624
Matthew Glinski <matthewglinski@gmail.com>
2015-02-24 15:25:12 -0500

    Calculate the actual time an IP address is banned on the API, and use that timer instead of 30 minutes.

--- app/eveapi/account/AccountAPIKeyInfo.php
+++ app/eveapi/account/AccountAPIKeyInfo.php
@@ -76,7 +76,11 @@ class APIKeyInfo extends BaseApi
                     // If we are rate limited, set the status of the eveapi
                     // server to 'down' in the cache so that subsequent
                     // calls don't fail because of this.
-                    \Cache::set('eve_api_down', true, 30);
+
+	                // Get time of IP ban in minutes, rounded up to the next whole minute
+	                $time = round(($e->cached_until_unixtime - $e->request_time_unixtime) / 60, 0, PHP_ROUND_HALF_UP);
+
+	                \Cache::set('eve_api_down', true, $time);
                     return;
 
                 // "EVE backend database temporarily disabled.""
